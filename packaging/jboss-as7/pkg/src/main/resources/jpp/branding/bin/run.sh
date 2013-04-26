38e9b4058a43ff311e1aaa4520f37f4519742cee
bdaw <boleslaw.dawidowicz@gmail.com>
2013-04-26 09:42:09 +0200

    JPP Packaging Resources

---
+++ packaging/jboss-as7/pkg/src/main/resources/jpp/branding/bin/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Placeholder for people used to run.sh from older version redirecting them to read the readme
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo 
+echo ========================================================================================
+echo 
+echo To start JBoss Portal Platform 6 Standalone Server use the command:
+echo
+echo  $DIR/standalone.sh
+echo
+echo
+echo ========================================================================================
+echo
