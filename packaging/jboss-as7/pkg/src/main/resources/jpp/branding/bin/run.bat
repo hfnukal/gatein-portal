961ba278dc417ebd1ea99b2f4f0c3eae2179a1e9
bdaw <boleslaw.dawidowicz@gmail.com>
2013-04-26 09:42:09 +0200

    JPP Packaging Resources

---
+++ packaging/jboss-as7/pkg/src/main/resources/jpp/branding/bin/run.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+REM Placeholder for people used to run.bat from older version redirecting them to read the readme
+echo.
+echo =======================================================================================
+echo.
+echo To start JBoss Portal Platform 6 Standalone Server use the command:
+echo.
+echo    %CD%\standalone.bat
+echo.
+echo.
+echo ========================================================================================
+echo.
